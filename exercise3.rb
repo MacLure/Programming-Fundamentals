5c02841b61ffe90ea2240e5be9a436ecc1732f98
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2018-11-12 13:46:40 -0500

    complete exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,2 +1,9 @@
-puts "What is your name?"
+require 'date'
 
+puts "What is your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}"
+
+puts "How old are you?"
+user_age = gets.chomp
+puts "I see, #{user_name}.  You were born in #{Date.today.strftime("%Y").to_i - (user_age.to_i)}."
